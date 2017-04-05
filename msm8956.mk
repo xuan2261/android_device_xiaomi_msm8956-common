33bc5f9526f139e841afbbbdbf38d90d5bcdf8fd
Jaekyun Seok <jaekyun@google.com>
2017-04-05 11:23:26 +0900

    msm8956-common: Enforce RRO on framework-res
    Overlays only for framework-res will be converted into RROs.
    
    Other overlays can't be converted due to some known issues on app RRO.
    
    Bug: 36231603
    Change-Id: Idca30fdbbcc990fd124de16a06a112a346612a61
    (cherry picked from commit db1b6058200fc5255f519cf6609fca7619d6743e)

--- msm8956.mk
+++ msm8956.mk
@@ -20,6 +20,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-mokee
 
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
+
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/msm8956-common/msm8956-common-vendor.mk)
 
