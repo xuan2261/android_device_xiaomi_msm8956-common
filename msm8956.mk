d6923f62abeb5d28d0da2e26317b3e3b51073bff
GuaiYiHu <guaiyihu@foxmail.com>
2019-06-08 12:18:14 +0800

    msm8956-common: Switch to hidl consumerir service
    Change-Id: I9acf14de923ad879c989f3d83b6d963d3323c8b0

--- msm8956.mk
+++ msm8956.mk
@@ -128,8 +128,7 @@ PRODUCT_PACKAGES += \
 
 # Consumerir
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    consumerir.msm8952
+    android.hardware.ir@1.0-service.xiaomi_msm8956
 
 # DataServices
 PRODUCT_PACKAGES += \
